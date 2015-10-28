44d609247767fc2940f49fad5604ffdd643e39dc
Piotr Klimiec <klimiec@gmail.com>
2015-10-28 22:12:27 +0100

    Ultrasonic distance measurement

--- python/gpio.py
+++ python/gpio.py
@@ -3,25 +3,23 @@ import time
 
 class PINS:
 
+# WC1 
 	WC1_LED_RED = 2
 	WC1_LED_GREEN = 3
 	WC1_DOOR_sensor = 10
+	WC1_PIR = 9
 
-	PIR = 19
-	TRIG = 20
-	ECHO = 21
-	RELAY = 16
-
+# WC2
 	WC2_LED_RED = 4
 	WC2_LED_GREEN = 17
 
+# URINAL
 	URINAL_LED_RED = 27
 	URINAL_LED_GREEN = 22
-
+	URINAL_TRIG = 11
+	URINAL_ECHO = 0
 
 	def __init__(self):
-
-		GPIO.cleanup()
 		GPIO.setmode(GPIO.BCM)
 		GPIO.setwarnings(False)
 
@@ -29,6 +27,9 @@ class PINS:
 		GPIO.setup(self.WC1_LED_RED, GPIO.OUT)
 		GPIO.setup(self.WC1_LED_GREEN, GPIO.OUT)
 		GPIO.setup(self.WC1_DOOR_sensor, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+		GPIO.setup(self.WC1_PIR, GPIO.IN)
+		# Relay Board
+		#GPIO.setup(self.RELAY, GPIO.OUT)
 
 		# WC2
 		GPIO.setup(self.WC2_LED_RED, GPIO.OUT)
@@ -37,68 +38,9 @@ class PINS:
 		# URINAL
 		GPIO.setup(self.URINAL_LED_RED, GPIO.OUT)
 		GPIO.setup(self.URINAL_LED_GREEN, GPIO.OUT)
+		GPIO.setup(self.URINAL_TRIG, GPIO.OUT)
+		GPIO.setup(self.URINAL_ECHO, GPIO.IN)
 		
-		#PIR
-		GPIO.setup(self.PIR, GPIO.IN)
-
-		# Relay Board
-		GPIO.setup(self.RELAY, GPIO.OUT)
-		print 'Constructor end'
-		
-		#Czujnik odlegosci
-		GPIO.setup(self.TRIG, GPIO.OUT)#TRIG
-		GPIO.setup(self.ECHO, GPIO.IN)#ECHO	
-
-	def odleglosc(self):
-		GPIO.output(self.TRIG, False)
-		time.sleep(2)
-		GPIO.output(self.TRIG, True)
-		time.sleep(0.00001)
-		GPIO.output(self.TRIG, False)
-
-		while GPIO.input(self.ECHO) == 0:
-			pulse_start = time.time()
-
-		while GPIO.input(self.ECHO) == 1:
-			pulse_end = time.time()
-
-		pulse_duration = pulse_end - pulse_start
-		distance = pulse_duration * 17150
-		distance = round(distance, 2)
-		
-		print 'Distance: ', distance
-		
-		
-		
-		
-	def is_motion_detected(self):
-		if GPIO.input(self.PIR):
-			print 'Move Detected'
-			# return True
-		else:
-			print '# clean'
-			# return False
-
-	def test(self):
-		print 'LOW'
-		GPIO.output(self.RELAY, GPIO.LOW)
-		time.sleep(5)
-		print 'HIGH'
-		GPIO.output(self.RELAY, GPIO.HIGH)
-		time.sleep(5)
-		print 'LOW'
-		GPIO.output(self.RELAY, GPIO.LOW)
-		time.sleep(5)
-		print 'HIGH'
-		GPIO.output(self.RELAY, GPIO.HIGH)
-		time.sleep(2)
-		print 'LOW'
-		GPIO.output(self.RELAY, GPIO.LOW)
-		time.sleep(2)
-		print 'HIGH'
-		GPIO.output(self.RELAY, GPIO.HIGH)
-
-# Tested
 	def wc1_led_occupied(self):
 		#toilet occupied
 		print 'WC1 - LED Red'
@@ -144,38 +86,30 @@ class PINS:
 			print '[False] Door WC1 open: ', GPIO.input(self.DOOR_WC1_sensor)
 			return False
 
-	def isDoorWc2Closed(self):
-		return True
-	def isMoveDetectedInWc2(self):
-		return True
+	def is_wc1_motion_detected(self):
+		if GPIO.input(self.WC1_PIR) == 1:
+			print 'Move Detected: ',GPIO.input(self.WC1_PIR)
+			return True
+		else:
+			print '# clean: ', GPIO.input(self.WC1_PIR)
+			return False
 
-	def turnOnLedWc1(self):
-		print 'LED in WC1 is turned on'
+	def get_distance(self):
+		GPIO.output(self.URINAL_TRIG, False)
+		time.sleep(0.3)
+		GPIO.output(self.URINAL_TRIG, True)
+		time.sleep(0.00001)
+		GPIO.output(self.URINAL_TRIG, False)
 
-	def turnOffLedWc1(self):
-		print 'LED in WC1 is turned off'
+		while GPIO.input(self.URINAL_ECHO) == 0:
+			pulse_start = time.time()
 
-	def turnOnLedWc2(self):
-		print 'LED in WC2 is turned on'
-		
-	def turnOffLedWc2(self):
-		print 'LED in WC2 is turned off'
+		while GPIO.input(self.URINAL_ECHO) == 1:
+			pulse_end = time.time()
 
-	def turnOnLedUrinal(self):
-		print 'LED in Urinal is turned on'
-		
-	def turnOffLedUrinal(self):
-		print 'LED in Urinal is turned off'
+		pulse_duration = pulse_end - pulse_start
+		distance = pulse_duration * 17150
+		distance = round(distance, 2)
+		return distance
+		print 'Distance: ', distance
 
-	def turnOnFun(self):
-		print 'Fun in wc1 is turned on'
-
-	def turnOffFun(self):
-		print 'Fun in wc1 is turned off'
-
-	def getDistance(self):
-		return 40
-
-
-# Test 
-g = PINS()
