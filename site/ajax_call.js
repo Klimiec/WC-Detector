1ea7d922cbfe29bc57c8ca09c19798fa3f7328ad
Piotr Klimiec <piotrek@Piotrs-Mac-mini.local>
2015-11-08 22:58:29 +0100

    perform ajax call checking state of WCs every 1 sec

--- site/ajax_call.js
+++ site/ajax_call.js
@@ -1,4 +1,50 @@
 $(document).ready(function() {
+    setInterval("ajaxd()",1000);
+});
+
+
+function ajaxd() { 
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:8181/toilets",
+    dataType: 'json',
+   success: function(data){
+            if (data.wc1 == 'Free') {
+                $('#wc1').attr("class", "success");
+                $('#wc1state').text("Free");
+                //alert('IF1 - free');  
+            } else {
+                $('#wc1').attr("class", "danger");
+                $('#wc1state').text("Occupied");
+                //alert('ELSE - occupied');  
+            }
+
+            if (data.urinal == 'Free') {
+                $('#urinal').attr("class", "success");
+                $('#urinalstate').text("Free");
+            } else {
+                $('#urinal').attr("class", "danger");
+                $('#urinalstate').text("Occupied");
+            }
+
+            if (data.wc2 == 'Free') {
+                $('#wc2').attr("class", "success");
+                $('#wc2state').text("Free");
+            } else {
+                $('#wc2').attr("class", "danger");
+                $('#wc2state').text("Occupied");
+            }
+   },
+    error: function (jqXHR, textStatus, errorThrown ) {
+              alert('Error!');  
+    }
+ });
+}
+
+
+/*
+$(document).ready(function() {
+
     $.ajax({
         type: "GET",
         url: "http://localhost:8181/toilets",
@@ -29,13 +75,12 @@ $(document).ready(function() {
                 $('#wc2').attr("class", "danger");
                 $('#wc2state').text("Occupied");
             }
-
-
-
-            //alert("OK");
         },
         error: function (jqXHR, textStatus, errorThrown ) {
               alert('Error: ' + jqXHR.responseText);  
         }
     });
-});
+}
+);
+
+*/
