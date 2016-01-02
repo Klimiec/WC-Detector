d82c0c969344e98de8661f58871f40bef339f132
Piotr Klimiec <klimiec@gmail.com>
2016-01-02 18:02:45 +0100

    remove self from door script

--- python/gpio_tutorial/door_sensor.py
+++ python/gpio_tutorial/door_sensor.py
@@ -2,13 +2,13 @@
 
 import RPi.GPIO as GPIO
 import time
-
+GPIO.setmode(GPIO.BCM)
 DOOR_sensor = 10
 GPIO.setup(DOOR_sensor, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-	if GPIO.input(self.DOOR_WC1_sensor) != 1:
-		print 'Door closed: ', GPIO.input(self.DOOR_WC1_sensor)
+	if GPIO.input(DOOR_sensor) != 1:
+		print 'Door closed: ', GPIO.input(DOOR_sensor)
 	else:
-		print 'Door open: ', GPIO.input(self.DOOR_WC1_sensor)
+		print 'Door open: ', GPIO.input(DOOR_sensor)
 
