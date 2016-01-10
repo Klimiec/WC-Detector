7e52d20760e3ed976e385d4df4b5fa1ae687969c
Piotr Klimiec <klimiec@gmail.com>
2016-01-10 23:51:20 +0100

    test script for door1 based on events

---
+++ python/gpio_tutorial_events/door_wc1_event.py
@@ -0,0 +1,45 @@
+
+import RPi.GPIO as GPIO
+import time
+import logging 
+import threading 
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='(%(threadName)-10s) %(message)s',
+                    )
+
+
+WC1_DOOR_sensor = 10
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(WC1_DOOR_sensor, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+lock = threading.Lock()
+counter = 0
+
+
+def closed_callback(channel): 
+	global lock
+	with lock:
+		global counter
+		counter += 1
+	    logging.debug('@WC1 Door CLOSED counter:  %s ', counter)
+
+
+def opened_callback(channel): 
+	global lock
+	with lock:
+		global counter
+		counter += 1
+		logging.debug('@WC1 Door OPENED counter:  %s ', counter)
+
+# 0->1 
+GPIO.add_event_detect(WC1_DOOR_sensor, GPIO.RISING, callback=opened_callback)  
+# 1->0
+GPIO.add_event_detect(WC1_DOOR_sensor, GPIO.FALLING, callback=closed_callback)  
+
+
+# Main Loop
+while True:
+	pass
