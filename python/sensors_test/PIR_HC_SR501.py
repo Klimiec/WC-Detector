550438cf8929c70b08172ee2247ff22c5f450930
Piotr Klimiec <klimiec@gmail.com>
2016-04-09 18:19:06 +0200

    test script for PIR HC-SR501

---
+++ python/sensors_test/PIR_HC_SR501.py
@@ -0,0 +1,17 @@
+
+
+import RPi.GPIO as GPIO
+import time
+
+PIR_OUT = 2
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIR_OUT, GPIO.IN)
+
+while True:
+	if GPIO.input(PIR_OUT) == 1:
+		print 'Move Detected: ',GPIO.input(PIR_OUT)
+	else:
+		print '# clean'
+	time.sleep(0.5)
+
