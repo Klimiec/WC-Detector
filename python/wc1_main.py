b71d96ca056fcee7fd1094f377f930504545c38b
Piotr Klimiec <klimiec@gmail.com>
2016-04-14 23:47:15 +0200

    fix debug messages

--- python/wc1_main.py
+++ python/wc1_main.py
@@ -17,82 +17,91 @@ gpio = Sensors()
 
 
 def wc1_worker(state, gpio):
-	logging.debug('@Wc_1 monitoring in other thread, current state of the system:  %s', state)
 
-	# Check if door are closed for duration of 4 seconds
-	is_door_open = False
-	start = time.time()
-	end = time.time()
+	#turn on red LED and send REST?
 
-	for i in range(10):
-		logging.debug('@Wc_1 monitoring iteracja door:  %s', i)
+	# Check if door are closed for duration of 2 seconds
+	start_first_check = time.time()
+
+	logging.debug('#STEP 1 ------------')
+	for i in range(40):
+		logging.debug('@Wc_1 door closed monitoring, iteration:  %s', i)
 		if gpio.is_wc1_door_closed() == False:
-			is_door_open = True
-			end = time.time()
-			logging.debug('Otwarto drzwi WC1 po takim czasie!! :  %s', round((end - start), 2))
+			logging.debug('@Wc_1 door opened, stop wc1 thread, wc is free :  %s', i)
+			state = False
+			return
 		else:
-			logging.debug('Czy drzwi WC1 sa zamkniete?:  %s', gpio.is_wc1_door_closed())
-			end = time.time()
-			time.sleep(0.1)
+			time.sleep(0.05)
 
-	pulse= end - start
-	distance = round(pulse, 2)
-	logging.debug('WC1 Czas trwaia 10 iteracji:  %s', distance )
-	logging.debug('WC1 Czy drzwi zostaly otwarte w ciagu 10 iteracji?: %s',  is_door_open)
+	loop_time = time.time() - start_first_check;
+	logging.debug('------------ #STEP 1 | @Wc_1 door closed monitoring total time:  %s', loop_time)
 
-	is_motion_detected = False
-	if is_door_open == True:
-		pass
-	else:
-		# Check if WC is still ocupied after 4 seconds
-		for i in range(10):
-			logging.debug('@Wc_1 monitoring iteracja PIR:  %s', i)
-			a = time.time()
-			if gpio.is_wc1_motion_detected():
-				is_motion_detected = True
-				logging.debug('@Wc_1 Wykryto ruch w iteracji PIR:  %s', i)
-				b = time.time()
-				logging.debug('@Wc_1 Czas wykrycia ruchu! :  %s', round((b - a), 2))
-				break
-			else:
-				time.sleep(0.1)
 
-	if is_door_open == True or is_motion_detected == False:
-		# change LED
-		gpio.wc1_led_free()
-		# send REST
-		state['wc1'] = False
-		rest.wc1_free()
-	else:
-		while True:
-			if gpio.is_wc1_door_closed() == False:
-				logging.debug('Wc_1 is now Free - change state')
-				# change LED
-				gpio.wc1_led_free()
-				# send REST
-				#time.sleep(6)
-				state['wc1'] = False
-				rest.wc1_free()
-				# trun on fun for 30 sek
-				gpio.wc1_fun_on()
-				time.sleep(30)
-				logging.debug('Wc_1 Fun is on for 30 sec .....')
-				# turn off fun 
-				gpio.wc1_fun_off()
-				break
-			else:
-				time.sleep(0.05)
+
+	# Check if there is a move for 10 second and door are still closed
+	start_second_check = time.time()
+
+	logging.debug('#STEP 2 ------------')
+	for i in range(200):
+		if gpio.is_wc1_door_closed() and gpio.is_wc1_motion_detected_by_Microwave():
+			logging.debug('@Wc_1 move detected, turn on red LED and send REST :  %s', i)
+			state = True;
+			gpio.wc1_led_occupied();
+			# send REST
+			break
+		elif gpio.is_wc1_door_closed() == False:
+			 logging.debug('@Wc_1 door opened, stop procedure, iteration: %s', i)
+			 state = False
+			 return
+		else:
+			time.sleep(0.05)
+
+	loop_time = time.time() - start;
+	logging.debug('#Step 2 | @Wc_1 door move detection monitoring total time:  %s', loop_time)
+
+
+	# Wait foor door to be open 
+	start_third_check = time.time()
+	last_time_move_detected = time.time()
+	
+	logging.debug('#Step 3 ------------')
+	while True:
+
+		no_move_time = round((time.time() - last_time_move_detected), 2)
+		if gpio.is_wc1_door_closed() == False:
+			logging.debug('@Wc_1 door opened, stop procedure, stop time: %s, total time: %s',time.time(), round((time.time() - start_third_check), 2))
+			# Otwarto drzwi
+			gpio.wc1_led_free();
+			# send REST
+			state = False
+			return
+		elif gpio.is_wc1_motion_detected_by_Microwave():
+			  logging.debug('@Wc_1 move detected after %s seconds ,keep going...', round((time.time() - last_time_move_detected), 2))
+			  last_time_move_detected = time.time()
+		elif no_move_time > 3:
+			logging.debug('@Wc_1 no move detected for 3 minutes, stop procedure')
+			# send REST
+			state = False
+
+			while True:
+				if gpio.is_wc1_door_closed():
+					gpio.wc1_led_probably_free()
+				else:
+		else:
+			time.sleep(0.05)
+
 
 
 # Main Loop of the program
 while True:
 	if  WC1_OCCUPIED == False and gpio.is_wc1_door_closed() and gpio.is_wc1_motion_detected_by_Microwave():
 		# change state of the WC1 to occupied
-		logging.debug('#Main Thread: change state of the WC1 (Free --> Occupied)')
+		logging.debug('-----------------------------------------------------------------')
+		logging.debug('#Main Thread |  change state of the WC1 (Free --> Occupied) | time %s', time.time())
 		WC1_OCCUPIED = True
 		# start new thread
 		t = threading.Thread(target=wc1_worker, args=(WC1_OCCUPIED,gpio,))
 		t.start()
 	else:
-		time.sleep(0.5)
+		time.sleep(1)
 
