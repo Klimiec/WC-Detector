d5150748a537bd5f7ef6bc0dc6c564165e9d05c4
Piotr Klimiec <klimiec@gmail.com>
2016-04-17 23:40:50 +0200

    script for measure PIR sensor delay in WC1

---
+++ python/sensors_test/delay_for_PIR_wc1.py
@@ -0,0 +1,14 @@
+
+import logging 
+import time
+import RPi.GPIO as GPIO
+from sensors import Sensors
+
+gpio = Sensors()
+GPIO.wait_for_edge(gpio.WC1_DOOR_sensor, GPIO.RISING)
+
+open_door_time = time.time()
+while gpio.is_wc1_motion_detected_by_PIR():
+	pass
+
+logging.debug('@Wc_1 duration of move detection after door opened: %s sec', round((time.time() - open_door_time), 2))
