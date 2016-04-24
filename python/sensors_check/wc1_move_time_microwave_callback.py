d144c46aba6434090b17a7c33f8e5d24ab53e4ea
Piotr Klimiec <klimiec@gmail.com>
2016-04-24 21:19:39 +0200

    test script for wc1, measure time from last time move detected, MicroWave sensor - polling and callback version

--- python/sensors_check/wc1_move_time_microwave_callback.py
+++ python/sensors_check/wc1_move_time_microwave_callback.py
@@ -1,14 +1,15 @@
 
 from sensors import Sensors
+import RPi.GPIO as GPIO
 import time
 
 gpio = Sensors()
 detection_counter = 0
-last_time_move_detected= time.time() 
 
+GPIO.add_event_detect(GPIO.WC1_MOVE_sensor, GPIO.FALLING)
 
 while True:
-	if gpio.is_wc1_motion_detected_by_Microwave():
+	if GPIO.event_detected(gpio.WC1_MOVE_sensor):
 		detection_counter += 1
 		time_elapsed = time.time() - last_time_move_detected
 		last_time_move_detected = time.time() 
@@ -16,3 +17,6 @@ while True:
 	else:
 		print '# clean'
 	time.sleep(0.5)
+
+
+	
