3a7e843902167ef4fa7596f92b27fd6477cb81a8
Piotr Klimiec <klimiec@gmail.com>
2016-04-24 22:15:40 +0200

    test script for door sensor for wc1 and w2 - polling

---
+++ python/sensors_check/wc1_door_sensor_polling.py
@@ -0,0 +1,18 @@
+
+
+from sensors import Sensors
+import time
+
+gpio = Sensors()
+detection_counter = 0
+
+
+while True:
+	if gpio.is_wc1_door_closed():
+		detection_counter += 1
+		print "Time: %s, | Detection counter: %d " % (time.strftime("%H:%M:%S"), detection_counter)
+	else:
+		print '# clean'
+	time.sleep(0.5)
+
+
