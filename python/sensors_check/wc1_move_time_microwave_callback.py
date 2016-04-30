a47b4f6653b059d4a46b0b1e908c598c3ea4db57
Klimiec <klimiec@gmail.com>
2016-04-30 23:20:41 +0000

    fix test script for WC1, enhance logging

--- python/sensors_check/wc1_move_time_microwave_callback.py
+++ python/sensors_check/wc1_move_time_microwave_callback.py
@@ -6,17 +6,18 @@ import time
 gpio = Sensors()
 detection_counter = 0
 
-GPIO.add_event_detect(GPIO.WC1_MOVE_sensor, GPIO.FALLING)
+GPIO.add_event_detect(gpio.WC1_MOVE_sensor, GPIO.FALLING)
+last_time_move_detected = time.time()
 
 while True:
 	if GPIO.event_detected(gpio.WC1_MOVE_sensor):
 		detection_counter += 1
 		time_elapsed = time.time() - last_time_move_detected
 		last_time_move_detected = time.time() 
-		print "Time: %s, | Detection counter: %d | Last time move detected (time elapsed): %.2f" % (time.strftime("%H:%M:%S"), detection_counter, last_time_move_detected)
+		print "Time: %s, | Detection counter: %d | Last time move detected (time elapsed): %.2f" % (time.strftime("%H:%M:%S"), detection_counter, time_elapsed)
 	else:
 		print '# clean'
-	time.sleep(0.5)
+	time.sleep(1)
 
 
 	
--- python/sensors_check/wc1_move_time_pir_callback.py
+++ python/sensors_check/wc1_move_time_pir_callback.py
@@ -8,15 +8,15 @@ gpio = Sensors()
 detection_counter = 0
 last_time_move_detected= time.time() 
 
-GPIO.add_event_detect(GPIO.WC1_MOVE_sensor, GPIO.RISING)
+GPIO.add_event_detect(gpio.WC1_MOVE_sensor, GPIO.RISING)
 
 while True:
 	if GPIO.event_detected(gpio.WC1_MOVE_sensor):
 		detection_counter += 1
 		time_elapsed = time.time() - last_time_move_detected
 		last_time_move_detected = time.time() 
-		print "Time: %s, | Detection counter: %d | Last time move detected (time elapsed): %.2f" % (time.strftime("%H:%M:%S"), detection_counter, last_time_move_detected)
+		print "Time: %s, | Detection counter: %d | Last time move detected (time elapsed): %.2f | PIR state: %s" % (time.strftime("%H:%M:%S"), detection_counter, time_elapsed, GPIO.input(gpio.WC1_MOVE_sensor))
 	else:
-		print '# clean'
-	time.sleep(0.5)
+		print '# clean | PIR state: %s' % (GPIO.input(gpio.WC1_MOVE_sensor))
+	time.sleep(1)
 
