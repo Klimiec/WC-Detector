d258bfbf361f675e863bc24a876cd6474ac8dd35
Piotr Klimiec <klimiec@gmail.com>
2016-05-02 17:01:10 +0200

    add PIT state in logging message for pir callback 2 script

--- python/sensors_check/wc1_move_time_pir_callback_2.py
+++ python/sensors_check/wc1_move_time_pir_callback_2.py
@@ -6,16 +6,16 @@ import RPi.GPIO as GPIO
 
 gpio = Sensors()
 
-channel = GPIO.wait_for_edge(gpio.WC1_MOVE_sensor, GPIO_RISING, timeout=5000)
+channel = GPIO.wait_for_edge(gpio.WC1_MOVE_sensor, GPIO.RISING, timeout=5000)
 
 if channel is None:
     print('Timeout occurred')
 else:
-    print('Edge detected on channel', channel)
+    print('Edge detected on channel', channel, '| PIR state: ', GPIO.input(gpio.WC1_MOVE_sensor))
 
 time.sleep(5)
 
-channel = GPIO.wait_for_edge(gpio.WC1_MOVE_sensor, GPIO_RISING, timeout=5000)
+channel = GPIO.wait_for_edge(gpio.WC1_MOVE_sensor, GPIO.RISING, timeout=5000)
 
 if channel is None:
     print('Timeout occurred')
