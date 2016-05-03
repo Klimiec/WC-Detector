676762b5d1606308e283c5dab17e04e0b7404d05
Piotr Klimiec <klimiec@gmail.com>
2016-05-03 13:51:07 +0200

    test script foor wc door sensor, based on threaded callback

---
+++ python/sensors_check/wc1_door_sensor_threaded_callback.py
@@ -0,0 +1,36 @@
+
+
+import threading 
+import time
+import logging 
+from sensors import Sensors
+import RPi.GPIO as GPIO
+
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='(%(threadName)-10s) %(message)s',
+                    )
+
+
+# Global
+detection_counter = 0
+
+
+######### WC1 Worker #########
+
+def wc1_callback(channel):
+	global detection_counter
+	detection_counter += 1
+	logging.debug('   @Wc_1 door | new thread started at time:  %s | detection counter: %s', time.strftime("%H:%M:%S"), detection_counter)
+
+
+
+
+# WC1 event callback
+GPIO.add_event_detect(gpio.WC1_DOOR_sensor, GPIO.FALLING, callback=wc1_callback, bouncetime=250)
+
+# Main Loop of the program
+while True:
+        time.sleep(10)
+        pass
+
